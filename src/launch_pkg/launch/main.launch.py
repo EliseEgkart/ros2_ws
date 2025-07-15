844defdc176d536fd772f15e9f4b5fb49af20f79
Hyeongjin_Kim <ky942400@gmail.com>
2025-07-15 15:41:43 +0900

    main launch test

--- src/launch_pkg/launch/main.launch.py
+++ src/launch_pkg/launch/main.launch.py
@@ -1,37 +1,38 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 import os
 
-from ament_index_python.packages import get_package_share_directory
-
 def generate_launch_description():
-    camera_pkg_dir = get_package_share_directory('camera_perception_pkg')
-    decision_pkg_dir = get_package_share_directory('decision_making_pkg')
+    # 각 패키지 디렉토리 경로 가져오기
+    yolov8_pkg_dir = get_package_share_directory('perception_yolov8_pkg')
+    debug_pkg_dir = get_package_share_directory('debug_pkg')
+    usb_cam_pkg_dir = get_package_share_directory('usb_cam')
 
+    # 개별 런치 포함
+    yolov8_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(yolov8_pkg_dir, 'launch', 'multi_camera_yolov8.launch.py')
+        )
+    )
+
+    debug_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(debug_pkg_dir, 'launch', 'multi_camera_yolov8_debug.launch.py')
+        )
+    )
+
+    camera_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(usb_cam_pkg_dir, 'launch', 'multiple_camera.launch.py')
+        )
+    )
+
+    # LaunchDescription 객체에 모두 추가
     return LaunchDescription([
-        # 개별 노드 실행
-        Node(
-            package='decision_making_pkg',
-            executable='motion_planner_node',
-            name='motion_planner_node',
-            output='screen',
-            #parameters=[{'param_name': 'param_value'}],
-            #remappings=[('/old/topic', '/new/topic')]
-        ),
-
-        # camera_perception_pkg의 launch 파일 포함 실행
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(camera_pkg_dir, 'launch', 'camera_group.launch.py')
-            )
-        ),
-
-        # decision_making_pkg의 launch 파일 포함 실행
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(decision_pkg_dir, 'launch', 'path_planner.launch.py')
-            )
-        ),
+        camera_launch,
+        yolov8_launch,
+        debug_launch,
     ])
